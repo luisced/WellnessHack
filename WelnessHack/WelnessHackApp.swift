ff55121da252a1425fc6cd30d64bf434a22e3cca
Luis Cedillo M <luis.cedillo.maldonado@proton.me>
2025-11-13 21:25:37 +0100

    Initial Commit

---
+++ WelnessHack/WelnessHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelnessHackApp.swift
+//  WelnessHack
+//
+//  Created by Luis Cedillo on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WelnessHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
