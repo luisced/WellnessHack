7000d5a1c2546ee87afe841d8cfcbdd263c59420
José Isai Galvan Villegas <0263985@up.edu.mx>
2025-11-14 04:29:45 -0600

    bateria
    bateria clean

--- WelnessHack/UI/Components/BatteryChargingView.swift
+++ WelnessHack/UI/Components/BatteryChargingView.swift
@@ -43,7 +43,7 @@ struct BatteryChargingView: View {
         ZStack {
             // Spline background for battery charging animation
             if let url = Bundle.main.url(
-                forResource: "battery_charging_animation",
+                forResource: "battery_charging_animation_copy",
                 withExtension: "splineswift"
             ) {
                 SplineView(sceneFileURL: url)
